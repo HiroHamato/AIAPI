bdcdb99c6cf60855f05a888201f3137427cbf8c9
MayarGandalf <kanyewestlover9124@gmail.com>
2024-12-09 19:13:58 +0300

    Исправил ошибку при замене файлов

--- DL_AI-main/README.md
+++
@@ -1,1 +0,0 @@
-daphne -p 8000 DjangoTest.asgi:application
--- DL_AI-main/ai/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-
-def chat_view(request):
-    return render(request, 'ai/chat.html')
-
-def base_view(request):
-    return render(request, 'ai/base.html')
