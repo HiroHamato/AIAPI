4b53d2031c250fc0e1168ebe991bb6c8bff49762
HiroHamato <72993255+HiroHamato@users.noreply.github.com>
2025-01-29 13:30:57 +0300

    чистка

--- ai/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('ai/chat/', views.chat_view, name='chat_view'),
-    path('ai/decide-task/', views.decide_task_view, name='decide_task_view'),
-    path('ai/find-error/',views.find_error_view, name='find_error_view'),
-]
