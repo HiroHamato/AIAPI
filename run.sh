a903f67256d613e5cb15824f4f7b6d45942cce4e
HiroHamato <72993255+HiroHamato@users.noreply.github.com>
2025-02-02 13:08:13 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
+docker stop dl_ai_container
+docker rm dl_ai_container
 docker pull whmi/dl_ai_one_container
 docker run -p 80:80 -p 5432:5432 -p 8000:8000 --name dl_ai_container whmi/dl_ai_one_container
